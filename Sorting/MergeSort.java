bf1cbd96f5358158d7987c31b9345931d0e5b1eb
Ramroop26 <ramroop26io071@satiengg.in>
2025-08-25 23:28:11 +0530

    merge

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,7 +1,59 @@
-public class MergeSort{
+public class MergeSort{ 
+   
+    static int[] divide(int arr[]) {
+        if(arr.length<=1) return arr;
 
+        int mid=arr.length/2;
+        int left[]= new int[mid];
+        int right[]=new int[arr.length-mid];
 
-
-
+        for(int i=0;i<mid;i++) {
+            left[i]=arr[i];
+        }
+        
+        for(int i=mid;i<arr.length;i++){
+            right[i-mid]=arr[i];
+        }
+        left=divide(left);
+        right=divide(right);
+        return merge(left,right);
+    }
     
+    static int[] merge(int left[],int right[]) {
+        int result[]=new int[left.length+right.length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<left.length && j<right.length) {
+            if(left[i]<right[j]) {
+                result[k++]=left[i++];
+            }
+            else {
+                result[k++]=right[j++];
+            }
+            
+        }
+        while(i<left.length) {
+            result[k++]=left[i++];
+        }
+    while(j<right.length){
+            result[k++]=right[j++];
+        }
+
+        
+        return result; 
+    }
+    public static void main(String ar[])
+    {
+        int arr[]={2,4,1,0,3,5,8,9};
+        int ans[]=divide(arr);
+
+        for(int Ans : ans){
+            System.out.print(Ans +" ");
+        }
+
+        
+      
+    }
 }
+
